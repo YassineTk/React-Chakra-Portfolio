25740c9545d8f5d2ea1a0889dc90a3b6b5057f93
iyassinetkd <yassinetakedd@gmail.com>
2020-11-17 15:01:47 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import Home from "./components/Home";
-import Contact from "./components/Contact";
 import "./components/FontawesomeIcons";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <Switch>
         <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
