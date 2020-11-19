2e436ad0e34acb26b612735ce94f8bf47667d844
iyassinetkd <yassinetakedd@gmail.com>
2020-11-19 16:10:11 +0100

    switched dark

--- src/components/Mode.js
+++ src/components/Mode.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
 function Mode() {
-  const { colorMode, toggleColorMode } = useColorMode("dark");
-
   return (
     <div class="mode">
       <Button marginRight="2">
         <Link className="mode__link" to="/contact">
           <FontAwesomeIcon icon="envelope" />{" "}
         </Link>
-      </Button>{" "}
-      <Button className="app__mode" onClick={toggleColorMode}>
-        {colorMode === "light" ? "🌙" : "⚡"}
       </Button>{" "}
     </div>
   );
