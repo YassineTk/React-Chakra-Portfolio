202bf4a581d1c4f54b7f19f05b379f063c8c4c9a
iyassinetkd <yassinetakedd@gmail.com>
2020-11-19 11:32:34 +0100

    netlify routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <Switch>
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
-
         <Route path="/" component={Home} />
       </Switch>
     </Router>
