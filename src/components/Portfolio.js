1598000088b068e97b8e53f5a21174978f885391
iyassinetkd <yassinetakedd@gmail.com>
2020-11-19 10:19:09 +0100

    added project description & json

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,18 @@
 import React from "react";
 import Mode from "./Mode";
-import { Image, Button, Link } from "@chakra-ui/react";
+import {
+  Image,
+  Button,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+} from "@chakra-ui/react";
+import Projects from "../data/projects.json";
 
 function Portfolio() {
   return (
@@ -9,62 +21,41 @@ function Portfolio() {
 
       <div class="portfolio__header skills__title">PROJECTS</div>
       <div class="portfolio__items">
-        <div className="portfolio__item__1">
-          {" "}
-          <div className="portfolio__item__image">
-            <Image
-              borderRadius="full"
-              boxSize="150px"
-              src="./images/chatapp.png"
-              alt="Segun Adebayo"
-            />
-          </div>
-          <h4 className="portfolio__item__title">HxH Chat</h4>
-          <div className="portfolio__item__buttons">
-            <Link target="_blank" href="http://meetthemeteo.netlify.app/">
-              <Button marginTop="8px">Live Demo</Button>
-            </Link>{" "}
-            <Button marginTop="8px">Description</Button>{" "}
-          </div>
-        </div>
-        <div className="portfolio__item__2">
-          {" "}
-          <div className="portfolio__item__image">
-            <Image
-              borderRadius="full"
-              boxSize="150px"
-              src="./images/react.webp"
-              alt="Segun Adebayo"
-            />
-          </div>
-          <h4 className="portfolio__item__title">Meet The Meteo</h4>
-          <div className="portfolio__item__buttons">
-            <Link target="_blank" href="http://meetthemeteo.netlify.app/">
-              <Button marginTop="8px">Live Demo</Button>
-            </Link>{" "}
-            <Button marginTop="8px">Description</Button>{" "}
-          </div>
-        </div>
-
-        <div className="portfolio__item__3">
-          {" "}
-          <div className="portfolio__item__image">
-            <Image
-              borderRadius="full"
-              boxSize="150px"
-              src="./images/anisfitness.png"
-              alt="Segun Adebayo"
-            />
-          </div>
-          <h4 className="portfolio__item__title">Anis Fitness</h4>
-          <div className="portfolio__item__buttons">
-            <Link target="_blank" href="http://anisfitness.herokuapp.com/">
+        {Projects.map((project) => {
+          return (
+            <div id={project.id} className={project.class}>
               {" "}
-              <Button marginTop="8px">Live Demo</Button>{" "}
-            </Link>
-            <Button marginTop="8px">Description</Button>{" "}
-          </div>
-        </div>
+              <div className="portfolio__item__image">
+                <Image
+                  borderRadius="full"
+                  boxSize="150px"
+                  src={project.image}
+                />
+              </div>
+              <h4 className="portfolio__item__title">{project.name}</h4>
+              <div className="portfolio__item__buttons">
+                <Link target="_blank" href={project["demo-link"]}>
+                  <Button marginTop="8px">Live Demo</Button>
+                </Link>{" "}
+                <Popover>
+                  <PopoverTrigger>
+                    <Button>Description</Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader>{project.name}</PopoverHeader>
+                    <PopoverBody>{project.description}</PopoverBody>
+                    <PopoverBody>
+                      {project.techs} <br />
+                      {project.repo}
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
